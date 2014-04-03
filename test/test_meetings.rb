882bb57481fa794f1b30530a68776ad2bf77e7a1
Jacob Swanner <jacob@envylabs.com>
2014-04-03 18:49:45 -0400

    Add start meeting API endpoint

--- test/test_meetings.rb
+++ test/test_meetings.rb
@@ -7,17 +7,17 @@ class MeetingsTest < Test::Unit::TestCase
     setup do
       @c = GoToMeeting::Client.new("12345")
     end
-    
+
     teardown do
       FakeWeb.clean_registry
     end
-    
+
     should "generate valid get meeting" do
       FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2M/rest/meetings/54321", :body => '{"meetingid":"54321"}', :content_type => "application/json", :status => ["200", "OK"])
       @r = @c.get_meeting('54321')
       assert_not_nil @r
       assert @r.parsed_response.is_a?(Hash)
-      assert_equal('54321', @r.parsed_response["meetingid"]) 
+      assert_equal('54321', @r.parsed_response["meetingid"])
     end
 
     should "generate valid create meetings" do
@@ -25,9 +25,9 @@ class MeetingsTest < Test::Unit::TestCase
       @r = @c.create_meeting({"subject" => "this meeting"})
       assert FakeWeb.last_request.body.is_a?(String)
       assert_equal('{"subject":"this meeting"}', FakeWeb.last_request.body)
-      assert_equal('12345', @r.parsed_response) 
+      assert_equal('12345', @r.parsed_response)
     end
-    
+
     should "generate valid update meetings" do
       FakeWeb.register_uri(:put, "https://api.citrixonline.com/G2M/rest/meetings/12345", :content_type => "application/json", :status => ["204", "No Content"])
       @r = @c.update_meeting("12345", {"subject" => "this meeting"})
@@ -35,13 +35,21 @@ class MeetingsTest < Test::Unit::TestCase
       assert_equal('{"subject":"this meeting"}', FakeWeb.last_request.body)
       assert_equal("204", @r.response.code)
     end
-    
+
     should "generate valid delete meeting" do
       FakeWeb.register_uri(:delete, "https://api.citrixonline.com/G2M/rest/meetings/12345", :content_type => "application/json", :status => ["204", "No Content"])
       @r = @c.delete_meeting("12345")
       assert_equal("204", @r.response.code)
     end
-    
+
+    should "generate valid start meeting" do
+      FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2M/rest/meetings/12345/start", :body => '{"hostURL":"https://downloadstage.citrixonline.com/download.html?startMode=Join&meetingId=12345&authenticationToken=3A200"}', :content_type => "application/json", :status => ["200", "OK"])
+      @r = @c.start_meeting("12345")
+      assert_not_nil @r
+      assert @r.parsed_response.is_a?(Hash)
+      assert_not_nil @r.parsed_response["hostURL"]
+    end
+
     should "generate valid get meetings" do
       FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2M/rest/meetings?history=true&startDate=2011-11-15T09%3A00%3A00Z&endDate=2011-11-15T09%3A00%3A00Z", :body => '[{"meetingid":12345},{"meetingid": 54321}]', :content_type => "application/json", :status => ["200", "OK"])
       @r = @c.get_meetings({"history"=> "true", "startDate" => "2011-11-15T09:00:00Z", "endDate" => "2011-11-15T09:00:00Z"})
@@ -49,7 +57,7 @@ class MeetingsTest < Test::Unit::TestCase
       assert @r.parsed_response.is_a?(Array)
       assert_not_nil @r.parsed_response.first["meetingid"]
     end
-    
+
     should "generate valid get meetings by group" do
       FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2M/rest/groups/12345/meetings?history=true&startDate=2011-11-15T09%3A00%3A00Z&endDate=2011-11-15T09%3A00%3A00Z", :body => '[{"meetingid":12345},{"meetingid": 54321}]', :content_type => "application/json", :status => ["200", "OK"])
       @r = @c.get_meetings_by_group("12345", {"history"=> "true", "startDate" => "2011-11-15T09:00:00Z", "endDate" => "2011-11-15T09:00:00Z"})
@@ -57,7 +65,7 @@ class MeetingsTest < Test::Unit::TestCase
       assert @r.parsed_response.is_a?(Array)
       assert_not_nil @r.parsed_response.first["meetingid"]
     end
-    
+
     should "generate valid get meetings by organizer" do
       FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2M/rest/organizers/12345/meetings?history=true&startDate=2011-11-15T09%3A00%3A00Z&endDate=2011-11-15T09%3A00%3A00Z", :body => '[{"meetingid":12345},{"meetingid": 54321}]', :content_type => "application/json", :status => ["200", "OK"])
       @r = @c.get_meetings_by_organizer("12345", {"history"=> "true", "startDate" => "2011-11-15T09:00:00Z", "endDate" => "2011-11-15T09:00:00Z"})
@@ -65,7 +73,7 @@ class MeetingsTest < Test::Unit::TestCase
       assert @r.parsed_response.is_a?(Array)
       assert_not_nil @r.parsed_response.first["meetingid"]
     end
-    
+
   end
 end
 
